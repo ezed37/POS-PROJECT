0dba46de767deb18785885138ab7ad4e452b8889
ezed37 <tharindupmaduranga@gmail.com>
2025-12-10 17:18:50 +0530

    Logout added

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,4 +1,5 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
 import AppBar from "@mui/material/AppBar";
 import Toolbar from "@mui/material/Toolbar";
 import IconButton from "@mui/material/IconButton";
@@ -9,38 +10,76 @@ import MenuIcon from "@mui/icons-material/Menu";
 import Brightness4Icon from "@mui/icons-material/Brightness4";
 import Brightness7Icon from "@mui/icons-material/Brightness7";
 import AuthContext from "../auth/AuthContext";
+import Menu from "@mui/material/Menu";
+import MenuItem from "@mui/material/MenuItem";
 
 const Navbar = ({ toggleSidebar, toggleMode, mode }) => {
-  const { user } = useContext(AuthContext);
+  const { logout, user } = useContext(AuthContext);
+  const navigate = useNavigate();
+
+  const [anchorEl, setAnchorEl] = useState(null);
+  const open = Boolean(anchorEl);
+
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  const handleLogout = () => {
+    handleClose();
+    logout();
+    navigate("/login");
+  };
 
   return (
-    <AppBar position="fixed">
-      <Toolbar>
-        <IconButton
-          edge="start"
-          color="inherit"
-          aria-label="menu"
-          onClick={toggleSidebar}
-          sx={{ mr: 2 }}
-        >
-          <MenuIcon />
-        </IconButton>
-        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-          POS SYSTEM | ADMIN PANEL
-        </Typography>
-        <IconButton color="inherit" onClick={toggleMode}>
-          {mode === "dark" ? <Brightness7Icon /> : <Brightness4Icon />}
-        </IconButton>
-        <Switch
-          checked={mode === "dark"}
-          onChange={toggleMode}
-          color="default"
-        />
-        <Avatar alt="User Avatar" sx={{ ml: 2 }}>
-          {user?.username[0]?.toUpperCase() || "NN"}
-        </Avatar>
-      </Toolbar>
-    </AppBar>
+    <>
+      <AppBar position="fixed">
+        <Toolbar>
+          <IconButton
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            onClick={toggleSidebar}
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            POS SYSTEM | ADMIN PANEL
+          </Typography>
+          <IconButton color="inherit" onClick={toggleMode}>
+            {mode === "dark" ? <Brightness7Icon /> : <Brightness4Icon />}
+          </IconButton>
+          <Switch
+            checked={mode === "dark"}
+            onChange={toggleMode}
+            color="default"
+          />
+          <Avatar alt="User Avatar" sx={{ ml: 2 }} onClick={handleClick}>
+            {user?.username[0]?.toUpperCase() || "NN"}
+          </Avatar>
+        </Toolbar>
+      </AppBar>
+
+      <Menu
+        anchorEl={anchorEl}
+        open={open}
+        onClose={handleClose}
+        anchorOrigin={{
+          vertical: "bottom",
+          horizontal: "right",
+        }}
+        transformOrigin={{
+          vertical: "top",
+          horizontal: "right",
+        }}
+      >
+        <MenuItem onClick={handleLogout}>Logout</MenuItem>
+      </Menu>
+    </>
   );
 };
 
